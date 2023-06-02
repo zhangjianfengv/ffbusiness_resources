a7ecb90e701bd81256d1ae3a7251c59277115d9c
zhangjianfengv <525707264@qq.com>
2023-06-02 10:19:47 +0800

    找不到queryCurrent这个函数

--- src/components/AppHistory.vue
+++ src/components/AppHistory.vue
@@ -1,59 +1,64 @@
 <template>
   <div id="app">
-    <form id="queryForm">
-      <div class="input-group" id="queryParam">
-        <input class="form-control" id="itemId" placeholder="请输入完整物品ID" type="text" value="">
-        <input class="form-control" id="itemName" placeholder="请输入部分或完整物品名" type="text" value="">
-        <input class="form-control" id="buyerName" placeholder="请输入完整购买者角色名" type="text" value="">
-        <input class="form-control" id="date" placeholder="请选择或输入日期" type="text">
-        <select class="form-control" id="worldName">
-          <option value="摩杜纳">摩杜纳</option>
-          <option value="旅人栈桥">旅人栈桥</option>
-          <option value="琥珀原">琥珀原</option>
-          <!--            <option value="雪松原">雪松原</option>-->
-          <option value="拉诺西亚">拉诺西亚</option>
-          <option value="紫水栈桥">紫水栈桥</option>
-          <option value="延夏">延夏</option>
-          <option value="神意之地">神意之地</option>
-          <option value="红玉海">红玉海</option>
-          <!--            <option value="月牙湾">月牙湾</option>-->
-          <option value="柔风海湾">柔风海湾</option>
-          <option value="银泪湖">银泪湖</option>
-          <option value="伊修加德">伊修加德</option>
-          <option value="梦羽宝境">梦羽宝境</option>
-          <option value="红茶川">红茶川</option>
-          <option value="太阳海岸">太阳海岸</option>
-          <option value="宇宙和音">宇宙和音</option>
-          <option value="幻影群岛">幻影群岛</option>
-          <option value="白银乡">白银乡</option>
-          <option value="晨曦王座">晨曦王座</option>
-          <option value="海猫茶屋">海猫茶屋</option>
-          <option value="沃仙曦染">沃仙曦染</option>
-          <!--            <option value="黄金谷">黄金谷</option>-->
-          <option value="水晶塔">水晶塔</option>
-          <option value="萌芽池">萌芽池</option>
-          <option value="白金幻象">白金幻象</option>
-          <option value="拂晓之间">拂晓之间</option>
-          <option value="神拳痕">神拳痕</option>
-          <option value="龙巢神殿">龙巢神殿</option>
-          <option value="静语庄园">静语庄园</option>
-          <option value="潮风亭">潮风亭</option>
-          <option value="陆行鸟">陆行鸟</option>
-          <option value="猫小胖">猫小胖</option>
-          <option value="莫古力">莫古力</option>
-          <option value="豆豆柴">豆豆柴</option>
-          <option selected value="中国">中国</option>
-        </select>
-        <b-button class="btn btn-primary mx-1" @click="searchItem()" type="button">搜索</b-button>
-        <b-button class="btn btn-primary" @click="searchMarketable()" type="button">
-          畅销排行
-        </b-button>
-        <b-button class="btn btn-primary mx-1" @click="openSearchItem()" type="button">
-          物品查询
-        </b-button>
-        <b-button class="btn btn-primary" @click="resetQueryParams()" type="button">重置</b-button>
-      </div>
-    </form>
+    <b-form inline id="queryForm">
+      <b-col id="queryParam" cols="12">
+        <b-row no-gutters>
+          <b-col cols="8">
+            <b-form-input class="w-25" id="itemId" placeholder="请输入完整物品ID" type="text" value=""></b-form-input>
+            <b-form-input class="w-25" id="itemName" placeholder="请输入部分或完整物品名" type="text"
+                          value=""></b-form-input>
+            <b-form-input class="w-25" id="buyerName" placeholder="请输入完整购买者角色名" type="text"
+                          value=""></b-form-input>
+            <b-form-input class="w-25" id="date" placeholder="请选择或输入日期" type="text"></b-form-input>
+          </b-col>
+          <b-col cols="2">
+            <b-form-select class="w-100" id="worldName">
+              <option value="摩杜纳">摩杜纳</option>
+              <option value="旅人栈桥">旅人栈桥</option>
+              <option value="琥珀原">琥珀原</option>
+              <option value="拉诺西亚">拉诺西亚</option>
+              <option value="紫水栈桥">紫水栈桥</option>
+              <option value="延夏">延夏</option>
+              <option value="神意之地">神意之地</option>
+              <option value="红玉海">红玉海</option>
+              <option value="柔风海湾">柔风海湾</option>
+              <option value="银泪湖">银泪湖</option>
+              <option value="伊修加德">伊修加德</option>
+              <option value="梦羽宝境">梦羽宝境</option>
+              <option value="红茶川">红茶川</option>
+              <option value="太阳海岸">太阳海岸</option>
+              <option value="宇宙和音">宇宙和音</option>
+              <option value="幻影群岛">幻影群岛</option>
+              <option value="白银乡">白银乡</option>
+              <option value="晨曦王座">晨曦王座</option>
+              <option value="海猫茶屋">海猫茶屋</option>
+              <option value="沃仙曦染">沃仙曦染</option>
+              <option value="水晶塔">水晶塔</option>
+              <option value="萌芽池">萌芽池</option>
+              <option value="白金幻象">白金幻象</option>
+              <option value="拂晓之间">拂晓之间</option>
+              <option value="神拳痕">神拳痕</option>
+              <option value="龙巢神殿">龙巢神殿</option>
+              <option value="静语庄园">静语庄园</option>
+              <option value="潮风亭">潮风亭</option>
+              <option value="陆行鸟">陆行鸟</option>
+              <option value="猫小胖">猫小胖</option>
+              <option value="莫古力">莫古力</option>
+              <option value="豆豆柴">豆豆柴</option>
+              <option selected value="中国">中国</option>
+            </b-form-select>
+          </b-col>
+          <b-button class="btn btn-primary mx-1" @click="searchItem()" type="button">搜索</b-button>
+          <b-button class="btn btn-primary" @click="searchMarketable()" type="button">
+            畅销排行
+          </b-button>
+          <b-button class="btn btn-primary mx-1" @click="openSearchItem()" type="button">
+            物品查询
+          </b-button>
+          <b-button class="btn btn-primary" @click="resetQueryParams()" type="button">重置</b-button>
+        </b-row>
+      </b-col>
+    </b-form>
     <div>
       <BootstrapTable id="table"
                       ref="table"
@@ -63,7 +68,6 @@
                       @on-post-body="vueFormatterPostBody"
       />
     </div>
-
     <div aria-hidden="true" aria-labelledby="myModalLabel" class="modal fade" id="myModal" role="dialog" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -82,14 +86,13 @@
     <div style="text-align:center;">
       <a href="http://www.beian.gov.cn/portal/registerSystemInfo"
          style="color: #bbb;font-size: 12px;text-decoration: none;"
-         target="_blank"><img src="/beian.png" style="width: 12px;height: 12px"/>
+         target="_blank"><img src="/beian.png" style="width: 12px;height: 12px" alt="公安机关联网备案"/>
         闽公网安备 35012102500734号
       </a>
       <a href="https://beian.miit.gov.cn/" style="color: #bbb;font-size: 12px;text-decoration: none;" target="_blank">闽ICP备2023003454号-1</a>
     </div>
   </div>
 </template>
-
 <script>
 import tableMixin from '../mixins/table'
 import $ from "jquery";
@@ -97,6 +100,125 @@ import $ from "jquery";
 let query = {
   worldName: '中国'
 };
+
+export function queryCurrent(name, id, itemName) {
+  $.ajax({
+    url: "/ffbusiness/currentData/queryParentWorld", async: true, method: "post",
+    data: JSON.stringify({worldName: name}),
+    contentType: "application/json", success: function (data) {
+      $('#myModalLabel').html(data.worldName + itemName + '低价')
+    }
+  });
+  $('#myModal').modal('show');
+  let $currentTable = $('#currentTable');
+  $currentTable.bootstrapTable('destroy');
+  $currentTable.bootstrapTable({
+    url: '/ffbusiness/currentData/queryCurrent',
+    pagination: "true",
+    columns: [{
+      field: 'worldName',
+      title: '服务器'
+    }, {
+      field: 'retainerName',
+      title: '雇员名'
+    }, {
+      field: 'hq',
+      formatter: function addButton(value) {
+        if (value === 'true') return '<img src="/hq.png"' +
+            ' decoding="async" width="16" height="16" alt="hq">';
+        else return '';
+      },
+      title: '高品质'
+    }, {
+      field: 'pricePerUnit',
+      title: '单价'
+    }, {
+      field: 'quantity',
+      title: '数量'
+    }, {
+      field: 'total',
+      title: '总计'
+    }], method: 'post',
+    queryParams: function () {
+      let paramCurrent = {};
+      paramCurrent.worldName = name;
+      paramCurrent.itemId = id;
+      return paramCurrent;
+    },
+    contentType: "application/json",
+    pageNumber: 1,//初始化加载第一页，默认第一页
+    pageSize: 10,
+    pageList: [20, 50]
+  });
+}
+
+function initTable() {
+  $('#table').bootstrapTable({
+    url: '/ffbusiness/saleHistory/realData',
+    pagination: "true",
+    sidePagination: "server",
+    columns: [{
+      field: 'itemId',
+      title: '物品ID'
+    }, {
+      field: 'itemName',
+      formatter: function iconFormatter(value, row) {
+        let url = window.location.protocol + '//' + window.location.host + '/icon/' + row.itemId + '.png';
+        return '<img src="' + url + '" decoding="async" width="32" height="32" alt="图标">&nbsp;&nbsp;' + value;
+      },
+      title: '物品名称'
+    }, {
+      field: 'pricePerUnit',
+      visible: false,
+      title: '单价'
+    }, {
+      field: 'quantity',
+      visible: false,
+      title: '数量'
+    }, {
+      field: 'sum',
+      formatter: function sumFormatter(value, row) {
+        return row.pricePerUnit + 'X' + row.quantity + '=' + value
+      },
+      title: '总计'
+    }, {
+      field: 'buyerName',
+      title: '购买者'
+    }, {
+      field: 'worldName',
+      title: '服务器'
+    }, {
+      field: 'hq',
+      visible: false,
+      title: '高品质'
+    }, {
+      field: 'timestamp',
+      title: '购买时间'
+    }, {
+      title: '操作',
+      width: 100,
+      formatter:
+          function operation(value, row) {
+            return ' <b-button onclick="queryCurrent(\'' + row.worldName + '\',' + row.itemId + ',\'' +
+                row.itemName +
+                '\')"' +
+                ' type="button">现价</>';
+          }
+    }], method: 'post',
+    contentType: "application/json",
+    queryParamsType: '',
+    queryParams: function (params) {
+      query.pageSize = params.pageSize;
+      query.pageNumber = params.pageNumber;
+      return query
+    },
+    showJumpto: true,
+    pageNumber: 1,//初始化加载第一页，默认第一页
+    pageSize: 10,
+    pageList: [20, 100, 200, 500, 1000],
+  });
+}
+
 export default {
   mixins: [tableMixin],
   data() {
@@ -146,15 +268,13 @@ export default {
         }, {
           title: '操作',
           width: 100,
-          formatter: (value, row) => {
-            return this.vueFormatter({
-              template: ' <b-button class="btn btn-primary" @click="queryCurrent(row)"' + '>现价</b-button>',
-              data: {row},
-              methods: {
-                queryCurrent: this.queryCurrent
+          formatter:
+              function operation(value, row) {
+                return ' <b-button onclick="queryCurrent(\'' + row.worldName + '\',' + row.itemId + ',\'' +
+                    row.itemName +
+                    '\')"' +
+                    ' type="button">现价</>';
               }
-            })
-          }
         }
       ],
       options: {
@@ -186,73 +306,27 @@ export default {
         buyerName: $('#buyerName').val(),
         timestamp: $('#date').val()
       };
-      $('#table').bootstrapTable({
-        url: '/ffbusiness/saleHistory/realData',
-        pagination: "true",
-        sidePagination: "server",
-        columns: [{
-          field: 'itemId',
-          title: '物品ID'
-        }, {
-          field: 'itemName',
-          formatter: function iconFormatter(value, row) {
-            let url = window.location.protocol + '//' + window.location.host + '/icon/' + row.itemId + '.png';
-            return '<img src="' + url + '" decoding="async" width="32" height="32" alt="图标">&nbsp;&nbsp;' + value;
-          },
-          title: '物品名称'
-        }, {
-          field: 'pricePerUnit',
-          visible: false,
-          title: '单价'
-        }, {
-          field: 'quantity',
-          visible: false,
-          title: '数量'
-        }, {
-          field: 'sum',
-          formatter: function sumFormatter(value, row) {
-            return row.pricePerUnit + 'X' + row.quantity + '=' + value
-          },
-          title: '总计'
-        }, {
-          field: 'buyerName',
-          title: '购买者'
-        }, {
-          field: 'worldName',
-          title: '服务器'
-        }, {
-          field: 'hq',
-          visible: false,
-          title: '高品质'
-        }, {
-          field: 'timestamp',
-          title: '购买时间'
-        }, {
-          title: '操作',
-          width: 100,
-          formatter:
-              function operation(value, row) {
-                return ' <button class="btn btn-primary" onclick="queryCurrent(\'' + row.worldName + '\',' + row.itemId + ',\'' + row.itemName +
-                    '\')"' +
-                    ' type="button">现价</button>';
-              },
-        }], method: 'post',
-        contentType: "application/json",
-        queryParamsType: '',
-        queryParams: function (params) {
-          query.pageSize = params.pageSize;
-          query.pageNumber = params.pageNumber;
-          return query
-        },
-        showJumpto: true,
-        pageNumber: 1,//初始化加载第一页，默认第一页
-        pageSize: 10,
-        pageList: [20, 100, 200, 500, 1000],
-      });
+      initTable();
       $('#table').bootstrapTable('refresh', {
         query: query
       });
+    },
+    resetQueryParams() {
+      $('#queryForm')[0].reset();
+      $('#table').bootstrapTable('destroy');
+      query = {
+        worldName: '中国'
+      };
+      let $worldName = $('#worldName');
+      $worldName.val('中国');
+      initTable()
     }
+  },
+  mounted() {
+    $(document).ready(function () {
+      $('#date').datepicker({language: 'zh-CN'});
+      initTable();
+    })
   }
 }
 
