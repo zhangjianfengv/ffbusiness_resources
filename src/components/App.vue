115ee8dc5f9aa0c3c0d5894e04b95a79aea4faa5
zhangjianfengv <525707264@qq.com>
2023-06-11 18:11:43 +0800

    init

---
+++ src/components/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+
+  <div id="app">
+
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">锤子攻略站</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#">Disabled</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <!-- 使用 router-link 组件来导航. -->
+    <!-- 通过传入 `to` 属性指定链接. -->
+    <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,42 +1,73 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import {BootstrapVue} from 'bootstrap-vue'
 import './plugins/table.js'
-import routes from './routes'
 
 import $ from 'jquery'
+import AppHistory from "@/components/AppHistory.vue";
+import AppMarketable from "@/components/AppMarketable.vue";
+import App from "@/components/App.vue";
 
 window.jQuery = $
 window.$ = $
 
+Vue.use(VueRouter)
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 // new Vue({
 //     render: h => h(History),
 //     methods: {}
 // }).$mount('#app')
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象。
+// 我们晚点再讨论嵌套路由。
+const routes = [
+    {path: '/', redirect: AppHistory},
+    {path: '/history', component: AppHistory},
+    {path: '/marketable', component: AppMarketable}
+]
 
-const app = new Vue({
-    el: '#app',
-    data: {
-        currentRoute: window.location.pathname
-    },
-    computed: {
-        ViewComponent() {
-            const matchingView = routes[this.currentRoute]
-            // return matchingView
-            //     ? require('./components/' + matchingView + '.vue')
-            //     : require('./pages/404.vue')
-            return require('./components/' + matchingView + '.vue')
-        }
-    },
-    render(h) {
-        return h(this.ViewComponent)
-    }
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+    routes // (缩写) 相当于 routes: routes
 })
 
-window.addEventListener('popstate', () => {
-    app.currentRoute = window.location.pathname
-})
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+new Vue({
+    render: h => h(App),
+    router
+}).$mount('#app')
 
+// 现在，应用已经启动了！
+
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         currentRoute: window.location.pathname
+//     },
+//     computed: {
+//         ViewComponent() {
+//             const matchingView = routes[this.currentRoute]
+//             // return matchingView
+//             //     ? require('./components/' + matchingView + '.vue')
+//             //     : require('./pages/404.vue')
+//             return require('./components/' + matchingView + '.vue')
+//         }
+//     },
+//     render(h) {
+//         return h(this.ViewComponent)
+//     }
+// })
+//
+// window.addEventListener('popstate', () => {
+//     app.currentRoute = window.location.pathname
+// })
+//
 
 
