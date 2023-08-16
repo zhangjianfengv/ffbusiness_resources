fd3e46ec88223343b853932745a56f6f5869bdea
zhangjianfengv <525707264@qq.com>
2023-08-16 19:13:44 +0800

    更新笔记

--- src/components/App.vue
+++ src/components/App.vue
@@ -9,17 +9,49 @@
             <b-nav-item to="/history">销售履历</b-nav-item>
             <b-nav-item to="/marketable">畅销排行</b-nav-item>
             <b-nav-item to="/item">物品查询</b-nav-item>
+            <b-button variant="info" type="button"
+                      @click="note()"><i class="bi bi-journal-text"></i></b-button>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
     </div>
     <router-view></router-view>
+    <div aria-hidden="true" aria-labelledby="note" class="modal fade" id="note" role="dialog" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel">更新笔记</h4>
+          </div>
+          <div class="modal-body">
+            <h5>
+              2023年8月16日
+            </h5>
+            <h6>
+              性能优化：优化了销售履历物品名模糊查询的速度。具体为当模糊查询的结果过多时会缓存总数的值，下次同样的关键词查询时不再计算总数，缓存值几天更新一次。
+            </h6>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" data-dismiss="modal" @click="closeNote()" type="button">关闭</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
-  name: "App"
+  name: "App",
+  methods: {
+    note() {
+      $('#note').modal('show');
+    },
+    closeNote() {
+      $('#note').modal('toggle');
+    }
+  }
 }
 </script>
 
