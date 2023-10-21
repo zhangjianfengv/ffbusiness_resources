ab69673377a6b4b295c8f6f53a5cb44c1014ca2d
zhangjianfengv <525707264@qq.com>
2023-10-21 20:48:53 +0800

    微调

--- src/components/AppCurrent.vue
+++ src/components/AppCurrent.vue
@@ -28,9 +28,9 @@
     <table id="currentTable"></table>
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-1 variant="info">展开最近销售履历</b-button>
+        <b-button block v-b-toggle.accordion-3 variant="info">切换最近销售履历</b-button>
       </b-card-header>
-      <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+      <b-collapse id="accordion-3" visible accordion="my-accordion" role="tabpanel">
         <b-card-body>
           <table id="historyTable"></table>
         </b-card-body>
