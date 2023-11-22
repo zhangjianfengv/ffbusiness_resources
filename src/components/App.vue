3cfa8db21d17b8a0019f2b8fcb92c7b6fd9e4d2d
zhangjianfengv <525707264@qq.com>
2023-11-22 18:07:14 +0800

    更新笔记

--- src/components/App.vue
+++ src/components/App.vue
@@ -10,14 +10,13 @@
             <b-nav-item to="/marketable">市场统计</b-nav-item>
             <b-nav-item to="/item">物品查询</b-nav-item>
             <b-nav-item to="/current">实时物价</b-nav-item>
-            <b-button variant="info" type="button"
-                      @click="note()"><i class="bi bi-clipboard-fill"></i></b-button>
+            <b-button type="button"
+                      @click="toLogin"><img src="/bt_white_76X24.png" alt="QQ登录"/></b-button>
             <b-button variant="info" type="button"
                       href="mailto:jianfengfj@foxmail.com?subject=%E5%BE%AE%E8%A7%82%E7%BB%8F%E6%B5%8E%E5%AD%A6%E5%8F%8D%E9%A6%88&body=%E8%AF%B7%E5%A1%AB%E5%86%99%E6%82%A8%E7%9A%84%E5%AE%9D%E8%B4%B5%E6%84%8F%E8%A7%81%E6%88%96%E5%BB%BA%E8%AE%AE">
               <i class="bi bi-envelope-at-fill"></i></b-button>
-
-            <b-button type="button"
-                      @click="toLogin"><img src="/bt_white_76X24.png" alt="QQ登录"/></b-button>
+            <b-button variant="info" type="button"
+                      @click="note()"><i class="bi bi-clipboard-fill"></i></b-button>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
@@ -36,15 +35,6 @@
             <!--            body begin-->
             <div>
               <h6>
-                2023年11月11日
-              </h6>
-              <span style="font-size: smaller">
-                1.物品查询页面可筛选可出售或者有高品质的物品<br>
-                2.修复了物品查询页面材料清单只到第三级素材的问题
-            </span></div>
-            <br>
-            <div>
-              <h6>
                 2023年11月12日
               </h6>
               <span style="font-size: smaller">
@@ -57,6 +47,14 @@
               </h6>
               <span style="font-size: smaller">
                 1. 修复了实时物价查询页面只看HQ开关的有时不起作用的问题
+            </span></div>
+            <br>
+            <div>
+              <h6>
+                2023年11月22日
+              </h6>
+              <span style="font-size: smaller">
+                1.QQ登录（开发中，腾讯尚未审核通过但要求放置登录按钮）
             </span></div>
             <!--            body end-->
           </div>
