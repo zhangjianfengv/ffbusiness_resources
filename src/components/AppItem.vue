7c140961399d49c60580a75b4b123be80dc7553c
zhangjianfengv <525707264@qq.com>
2024-01-06 11:27:05 +0800

    材料成本计算时切换区服

--- src/components/AppItem.vue
+++ src/components/AppItem.vue
@@ -400,7 +400,7 @@ export default {
         contentType: "application/json",
         data: JSON.stringify({id: parseInt(id.toString())}),
         success: function (data) {
-          vm.worldName = data;
+          vm.itemName = data.rows[0].name;
         }
       });
     }
