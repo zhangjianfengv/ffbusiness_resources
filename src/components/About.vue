fe01dc52ee89bd6e3ed3abb5b5cc225784050f22
zhangjianfengv <525707264@qq.com>
2024-03-29 00:11:28 +0800

    o(〃＾▽＾〃)o

--- src/components/About.vue
+++ src/components/About.vue
@@ -15,22 +15,16 @@
         @sliding-start="onSlideStart"
         @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <!--      <b-carousel-slide-->
-      <!--          caption="站长本人"-->
-      <!--          text="Nulla vitae elit libero, a pharetra augue mollis interdum."-->
-      <!--          img-src="https://picsum.photos/1024/480/?image=52"-->
-      <!--      ></b-carousel-slide>  -->
       <b-carousel-slide
-          caption="站长本人"
+          caption="o(〃＾▽＾〃)o"
           img-src="/1.jpg"
       ></b-carousel-slide>
       <b-carousel-slide
-          caption="站长本人"
+          caption="o(〃＾▽＾〃)o"
           img-src="/2.jpg"
       ></b-carousel-slide>
       <b-carousel-slide
-          caption="站长本人"
+          caption="o(〃＾▽＾〃)o"
           img-src="/3.jpg"
       ></b-carousel-slide>
     </b-carousel>
